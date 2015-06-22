de3bade6ee83d9ea115b287fec0d1a3c64b4abca
Edgardo Izquierdo <b0neetz@gmail.com>
2015-06-22 15:09:58 -0300

    +

--- App RamosHnos/RamosHnos/Capas/Entidades/InsumoEntity.cs
+++ App RamosHnos/RamosHnos/Capas/Entidades/InsumoEntity.cs
@@ -8,6 +8,13 @@ namespace RamosHnos.Capas.Entidades
 {
     class InsumoEntity
     {
+        public int idInsumo { get; set; }
+        public int rubro { get; set; }
+        public string marca { get; set; }
+        public string insumo { get; set; }
+        public string descripcion { get; set; }
+        public string stockMin { get; set; }
+        public bool estado { get; set; }
     }
 
 }
---
+++ App RamosHnos/RamosHnos/Capas/Negocio/InsumoB.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using RamosHnos.Capas.Entidades;
+using RamosHnos.Capas.Datos;
+
+namespace RamosHnos.Capas.Negocio
+{
+    class InsumoB
+    {
+        public static InsumoEntity InsertInsumo(InsumoEntity insumo)
+        {
+            try
+            {
+                MySQLDAL.CnxDB();
+
+                string query = @"INSERT INTO Insumos (rubro, marca, insumo, descripcion, stockMin, estado) 
+                                 VALUES (@rubro, @marca, @insumo, @descripcion, @stockMin, @estado);
+                                 SELECT LAST_INSERT_ID()";
+
+
+                MySqlCommand cmd = new MySqlCommand(query, MySQLDAL.sqlcnx);
+                //cmd.CommandText = query;
+
+                cmd.Parameters.AddWithValue("@rubro", insumo.rubro);
+                cmd.Parameters.AddWithValue("@marca", insumo.marca);
+                cmd.Parameters.AddWithValue("@insumo", insumo.insumo);
+                cmd.Parameters.AddWithValue("@descripcion", insumo.descripcion);
+                cmd.Parameters.AddWithValue("@stockMin", insumo.stockMin);
+                cmd.Parameters.AddWithValue("@estado", insumo.estado);
+
+
+                //cmd.ExecuteNonQuery();
+                insumo.idInsumo = Convert.ToInt32(cmd.ExecuteScalar());
+
+                MessageBox.Show("Insumo Guardado!");
+                MySQLDAL.DcnxDB();
+
+                return insumo;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+                throw;
+            }
+        }
+
+
+            public static CostoInsumoEntity InsertCosto(CostoInsumoEntity cinsumo)
+        {
+            try
+            {
+                MySQLDAL.CnxDB();
+
+                string query = @"INSERT INTO CostoInsumos (insumo, costo, fechaActualizacion, estado) 
+                                 VALUES (@insumo, @costo, @fechaActualizacion, @estado);
+                                 SELECT LAST_INSERT_ID()";
+
+
+                MySqlCommand cmd = new MySqlCommand(query, MySQLDAL.sqlcnx);
+                //cmd.CommandText = query;
+
+                cmd.Parameters.AddWithValue("@insumo", cinsumo.insumo );
+                cmd.Parameters.AddWithValue("@costo", cinsumo.costo);
+                cmd.Parameters.AddWithValue("@fechaActualizacion", cinsumo.fechaActualizacion);
+                cmd.Parameters.AddWithValue("@estado", cinsumo.estado);
+
+                cmd.ExecuteNonQuery();
+                //insumo.idInsumo = Convert.ToInt32(cmd.ExecuteScalar());
+
+                MessageBox.Show("Costo Insumo Guardado!");
+                MySQLDAL.DcnxDB();
+
+                return cinsumo;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+                throw;
+            }
+        }
+
+            public static void MostrarInsumos(DataGridView dgv)
+            {
+                try
+                {
+                    DataTable dt = new DataTable();
+                    MySQLDAL.CnxDB();
+                    
+                    string query = @"SELECT *
+                                     FROM Insumos";
+
+                    MySqlCommand cmd = new MySqlCommand(query, MySQLDAL.sqlcnx);
+
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                    da.Fill(dt);
+                                        
+                    dgv.DataSource = dt;
+
+                    MySQLDAL.DcnxDB();
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex);
+                    throw;
+                }
+            }
+    }
+}
