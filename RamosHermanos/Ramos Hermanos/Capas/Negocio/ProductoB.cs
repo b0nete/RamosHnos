20ee0131c6943d619c3e3b9f96deefc589fa8733
b0nete <b0neetz@gmail.com>
2015-09-09 23:22:30 -0300

    zurdoFactura

--- RamosHermanos/Ramos Hermanos/Capas/Negocio/ProductoB.cs
+++ RamosHermanos/Ramos Hermanos/Capas/Negocio/ProductoB.cs
@@ -228,5 +228,45 @@ namespace RamosHermanos.Capas.Negocio
                 throw;
             }
         }
+
+        public static ProductoEntity AddProductoDGV(DataGridView dgv, ProductoEntity producto)
+        {
+            try
+            {
+                MySQL.ConnectDB();
+
+                string query = @"SELECT P.idProducto, P.producto, PP.Precio
+                                 FROM Productos P
+                                 INNER JOIN precioProductos PP ON PP.Producto = P.idProducto
+                                 WHERE P.idProducto = @idProducto and PP.estado = 1";
+
+                MySqlCommand cmd = new MySqlCommand(query, MySQL.sqlcnx);
+
+                cmd.Parameters.AddWithValue("@idProducto", producto.idProducto);
+
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    dgv.Rows.Add(
+                    Convert.ToString(dr["idProducto"]),
+                    Convert.ToString(dr["producto"]),
+                    Convert.ToString(""),
+                    Convert.ToString(dr["precio"]),
+                    Convert.ToString(""));
+                }
+
+                dr.Close();
+                MySQL.DisconnectDB();
+
+                return producto;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+                throw;
+            }
+        }
     }
 }
